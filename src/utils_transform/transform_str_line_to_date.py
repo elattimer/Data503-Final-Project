0a2f83a59b9297009a25d71b5af37a9fcc653c10
Max Downer <mdowner@spartaglobal.com>
2025-09-26 16:11:27 +0100

    fix func so it return date object instead of datetime

--- src/utils_transform/transform_str_line_to_date.py
+++ src/utils_transform/transform_str_line_to_date.py
@@ -3,10 +3,10 @@ from datetime import datetime
 def transform_str_line_to_date(date_string: str):
     ''''
     input is format Thursday 1 August 2019 as a string
-    output is 2019-8-1 00:00:00 as a datetime object
+    output is 2019-08-10 as a date object
     '''
     space_position = date_string.index(" ")
     stripped_date_string = date_string[space_position:]
-    date_object = datetime.strptime(stripped_date_string, "%d %B %Y")
-    print(date_object)
+    datetime_object = datetime.strptime(stripped_date_string, "%d %B %Y")
+    date_object = datetime_object.date()
     return date_object
