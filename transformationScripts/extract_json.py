b17f1fb90b7f87212cf20c85a8f992e8000ddd4b
Aurora <aurora.m.hill@gmail.com>
2025-09-29 11:12:11 +0100

    Converted file into a function to be used in other files
    Signed-off-by: Aurora <aurora.m.hill@gmail.com>

--- transformationScripts/extract_json.py
+++ transformationScripts/extract_json.py
@@ -1,4 +1,3 @@
-#pragma once
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
@@ -8,37 +7,40 @@ import pandas as pd
 import csv
 import json
 
+def extract_json():
+    subscription_id = "cd36dfff-6e85-4164-b64e-b4078a773259"
+    resource_group = "data503"
+    location = "uksouth"
+    storage_account_name = "data503paulastorage"
+    account_url = f"https://{storage_account_name}.blob.core.windows.net"
 
-subscription_id = "cd36dfff-6e85-4164-b64e-b4078a773259"
-resource_group = "data503"
-location = "uksouth"
-storage_account_name = "data503paulastorage"
-account_url = f"https://{storage_account_name}.blob.core.windows.net"
-
-credential = DefaultAzureCredential()
+    credential = DefaultAzureCredential()
 
 
 
-resource_client = ResourceManagementClient(credential, subscription_id)
-storage_client = StorageManagementClient(credential, subscription_id)
+    resource_client = ResourceManagementClient(credential, subscription_id)
+    storage_client = StorageManagementClient(credential, subscription_id)
 
-container_name = "talent"
+    container_name = "talent"
 
-#blobs
-blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
+    #blobs
+    blob_service_client = BlobServiceClient(account_url=account_url, credential=credential)
 
-# Get the client for the container
-container_client = BlobServiceClient.get_container_client(self = blob_service_client, container=container_name)
+    # Get the client for the container
+    container_client = BlobServiceClient.get_container_client(self = blob_service_client, container=container_name)
 
-dict_df = []
-for blob in container_client.list_blobs():
-    #checks for only json files
-    if blob.name.endswith(".json"):
-        print(f"Loading {blob.name} into DataFrame")
-        blob_client = container_client.get_blob_client(blob)
-        data = blob_client.download_blob().readall()
-        parsed = json.loads(data)
-        df = pd.json_normalize(parsed)
-        dict_df.append(df)
+    dict_df = []
 
-print(len(dict_df))
+    for blob in container_client.list_blobs():
+        #checks for only json files
+        if blob.name.endswith(".json"):
+            print(f"Loading {blob.name} into DataFrame")
+            blob_client = container_client.get_blob_client(blob)
+            data = blob_client.download_blob().readall()
+            parsed = json.loads(data)
+            df = pd.json_normalize(parsed)
+            dict_df.append(df)
+
+    combined_df = pd.concat(dict_df, ignore_index=True, sort=False)
+
+    return combined_df
