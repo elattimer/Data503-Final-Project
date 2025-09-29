e526d2011b21323ff9df4c5b33c73bee657ae95e
EleanorrrG <egalbraith34@gmail.com>
2025-09-29 15:42:38 +0100

    Added tqdm to extract jsons - EG

--- transformationScripts/extract_json.py
+++ transformationScripts/extract_json.py
@@ -1,13 +1,13 @@
 
 import pandas as pd
 import json
+from tqdm import tqdm
 
 
 def extract_json(container_client):
 
     dict_df = []
-
-    for blob in container_client.list_blobs():
+    for blob in tqdm(container_client.list_blobs(), desc="Extracting JSONs"):
         #checks for only json files
         if blob.name.endswith(".json"):
             blob_client = container_client.get_blob_client(blob)
