a8b27ce7e0feae189eccf85a9ec04f998cdbb43d
Aurora <aurora.m.hill@gmail.com>
2025-09-29 14:57:13 +0100

    set up the big return for the strengths and weaknesses dataframes
    Signed-off-by: Aurora <aurora.m.hill@gmail.com>

--- transformationScripts/transform_json.py
+++ transformationScripts/transform_json.py
@@ -1,4 +1,12 @@
 from extract_json import extract_json
+from transform_finincial_support_self import transform_financial_support_self
+from transform_json_lists import *
+
 
 data = extract_json()
 
+def get_strengths_frame():
+    return transform_strengths(data)
+
+def get_weaknesses_frame():
+    return transform_weaknesses(data)
--- transformationScripts/transform_json_lists.py
+++ transformationScripts/transform_json_lists.py
@@ -9,12 +9,13 @@ def transform_strengths(data: pd.DataFrame) -> pd.DataFrame:
     first = True
     for index, row in data.iterrows():
 
+        #set data to datetime format d/m/y
         date_data = str(row["date"]).split('/')
         cleaned = [x for x in date_data if x and x.strip()]
         date = cleaned[0] +"/"+ cleaned[1] + "/" + cleaned[2]
         dt = datetime.strptime(date, "%d/%m/%Y")
 
-
+        #loop through strengths of each person
         for j in row["strengths"]:
             new_data = {
                 "name": [str(row["name"]).upper()],
@@ -41,10 +42,40 @@ def transform_strengths(data: pd.DataFrame) -> pd.DataFrame:
     return df_all
 
 
-# with open("13482.json", "r", encoding="utf-8-sig") as f:
-#     record = json.load(f)  # json.load for a single object
+def transform_weaknesses(data: pd.DataFrame) -> pd.DataFrame:
 
-# # Wrap in a list to make a DataFrame
-# df = pd.DataFrame([record])
-data = transform_strengths(extract_json())
-print(data)
+    df_all = pd.DataFrame()
+    first = True
+    for index, row in data.iterrows():
+        
+        #set data to datetime format d/m/y
+        date_data = str(row["date"]).split('/')
+        cleaned = [x for x in date_data if x and x.strip()]
+        date = cleaned[0] +"/"+ cleaned[1] + "/" + cleaned[2]
+        dt = datetime.strptime(date, "%d/%m/%Y")
+
+        #loop through strengths of each person
+        for j in row["weaknesses"]:
+            new_data = {
+                "name": [str(row["name"]).upper()],
+                "weakness": [j],
+                "date" : [dt.date()],
+            }
+            new_df = pd.DataFrame(new_data)
+            #check if already exists
+            if first == False:
+                # Extract the single row as a Series
+                row_series = new_df.iloc[0]
+
+                # Compare to the larger DataFrame
+                mask = (df_all[list(row_series.index)] == row_series).all(axis=1)
+                if mask.any():
+                    print("Row exists in the DataFrame: " + new_df.to_string())
+                else:
+                    df_all = pd.concat([df_all,new_df], ignore_index=True)
+            else:
+                first = False
+                df_all = pd.concat([df_all,new_df], ignore_index=True)
+
+
+    return df_all
