e115108c2b162fbf7291a86b21693d148ade0e6a
EleanorrrG <egalbraith34@gmail.com>
2025-09-30 15:25:49 +0100

    Transform.py formatted

---
+++ src/transform.py
@@ -0,0 +1,16 @@
+def transform(dict_of_dfs):
+    #transform applicants csv
+    #transform jsons
+    #transform course behaviour csv
+
+    #Set personID for applicants_df
+
+    #Create mapping df
+
+    #Create frequency dictionary
+
+    #Set person ids   (mappingdf,dictionary,df)
+
+    #Assemble dfs that match tables in ERD
+
+    return dict_of_dfs
