bef02df29f816b51c2dc3412baf243f8c9dca76a
Callum Bain <cbain@spartaglobal.com>
2025-09-30 16:41:13 +0100

    mapped course_id to behaviour_scores and also added a start date column so person_id can be mapped

--- transformationScripts/transform_csv_course_behaviours.py
+++ transformationScripts/transform_csv_course_behaviours.py
@@ -17,30 +17,68 @@ def transform_csv_course_behaviours(combined_df: pd.DataFrame) -> pd.DataFrame:
 
     # Creating datetime column
     course_df['start_date'] = combined_df['file_name'].apply(lambda x : pd.to_datetime(x[-14:-4]))
-
     course_df["class_number"] = combined_df["file_name"].apply(lambda x : int(x[-17:-15]))
     course_df.drop_duplicates(inplace=True)
     course_df.reset_index(drop=True, inplace=True)
     course_df['course_id'] = range(1, len(course_df) + 1)
     course_df = course_df[['course_id', 'subject_name', 'trainer_name', 'start_date', 'class_number']]
-    return course_df
 
-transform_csv_course_behaviours(combined_df)
+    return course_df
 
 
 def create_behaviour_dataframe(combined_df: pd.DataFrame) -> pd.DataFrame:
     # Creating the behaviour_scores dataframe
-    course_df = transform_csv_course_behaviours(combined_df)
+
+    course_id_map_dict = {
+    'Data_28_2019-02-18.csv': 1,
+    'Data_29_2019-03-04.csv': 2,
+    'Data_30_2019-04-08.csv': 3,
+    'Data_31_2019-05-20.csv': 4,
+    'Data_32_2019-07-22.csv': 5,
+    'Data_33_2019-08-05.csv': 6,
+    'Data_34_2019-08-19.csv': 7,
+    'Data_35_2019-09-23.csv': 8,
+    'Data_36_2019-10-28.csv': 9,
+    'Data_37_2019-11-18.csv': 10,
+    'Data_38_2019-12-16.csv': 11,
+    'Data_39_2019-12-30.csv': 12,
+    'Engineering_17_2019-02-18.csv': 13,
+    'Engineering_18_2019-04-01.csv': 14,
+    'Engineering_19_2019-04-29.csv': 15,
+    'Engineering_20_2019-05-27.csv': 16,
+    'Engineering_21_2019-07-15.csv': 17,
+    'Engineering_22_2019-07-22.csv': 18,
+    'Engineering_23_2019-08-12.csv': 19,
+    'Engineering_24_2019-09-16.csv': 20,
+    'Engineering_25_2019-09-23.csv': 21,
+    'Engineering_26_2019-10-28.csv': 22,
+    'Engineering_27_2019-11-25.csv': 23,
+    'Engineering_28_2019-12-16.csv': 24,
+    'Engineering_29_2019-12-30.csv': 25,
+    'Business_20_2019-02-11.csv': 26,
+    'Business_21_2019-03-18.csv': 27,
+    'Business_22_2019-04-15.csv': 28,
+    'Business_23_2019-05-20.csv': 29,
+    'Business_24_2019-07-15.csv': 30,
+    'Business_25_2019-07-29.csv': 31,
+    'Business_26_2019-08-12.csv': 32,
+    'Business_27_2019-09-16.csv': 33,
+    'Business_28_2019-10-21.csv': 34,
+    'Business_29_2019-11-18.csv': 35,
+    'Business_30_2019-12-30.csv': 36}
+
+    combined_df['start_date'] = combined_df['file_name'].apply(lambda x : pd.to_datetime(x[-14:-4]))
+    combined_df['course_id'] = combined_df['file_name'].map(course_id_map_dict)
     combined_df.fillna(0, inplace=True)
     combined_df.drop('file_name', axis=1, inplace=True)
 
     '''
-    Created a dataframe which keeps the name and trainer the same as the original dataframe.
+    Created a dataframe which keeps the name, trainer, course_id and start_date the same as the original dataframe.
     Each combination of trait and week (e.g., "Analytic_W1") becomes a row under the new 'trait_week' column.
     The score for that 'trait_week' is placed in the next column.
     '''
     df_melted = combined_df.melt(
-    id_vars=['name', 'trainer'],
+    id_vars=['name', 'trainer', 'course_id', 'start_date'],
     var_name='trait_week',
     value_name='score')
 
@@ -51,7 +89,7 @@ def create_behaviour_dataframe(combined_df: pd.DataFrame) -> pd.DataFrame:
     Changes df_melted back into wide form. Creating a column for each trait and assigning its score.  
     '''
     df_traits = df_melted.pivot_table(
-    index=['name', 'week'],
+    index=['course_id', 'name', 'week', 'start_date'],
     columns='trait',
     values='score').reset_index()
 
@@ -71,9 +109,4 @@ def create_behaviour_dataframe(combined_df: pd.DataFrame) -> pd.DataFrame:
     
     df_traits["name"] = df_traits["name"].apply(lambda x : x.upper())
 
-    
-
-    print(df_traits)
-    
-
-create_behaviour_dataframe(combined_df)
+    return df_traits
