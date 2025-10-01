2ca6953420e0c2e766fad5204a97c9d81ccc973a
Aurora <aurora.m.hill@gmail.com>
2025-10-01 14:23:52 +0100

    Final transform for weaknesses, strengths, tech_skills, and sparta_day
    Signed-off-by: Aurora <aurora.m.hill@gmail.com>

--- src/transform.py
+++ src/transform.py
@@ -32,6 +32,12 @@ def transform(dict_of_dfs):
     txts = dict_of_dfs["txt"]
     id_txts = set_person_id(txts,mapping_df,names_freq,course=False)
     id_txts = id_txts.drop(["name"],axis=1)
+    sparta_day_sql = id_txts.drop(columns=['psychometric_score','presentation_score','id'])
+    sparta_day_sql = sparta_day_sql.rename(columns={"date": "day_Date", "location":"day_location"})
+    #add sparta day to final dict
+    newDictTxt = {"sparta_day":sparta_day_sql}
+    finalDict.update(newDictTxt)
+
     #transform jsons
 
     ## Strengths
@@ -39,12 +45,12 @@ def transform(dict_of_dfs):
 
     #set strengths to have a person id and remove the names and dates
     id_strengths = set_person_id(strengthDf,mapping_df,names_freq,course=False)
-    id_strengths = id_strengths.drop(['name'],axis=1)
-    id_strengths = id_strengths.drop(['date'],axis=1)
-    id_strengths= pd.merge(strengthDf, id_txts, on=["id"])
-    id_strengths = id_strengths.drop(['date'],axis=1)
-    id_strengths = id_strengths.rename(columns={"id": "person_id"})
-    id_strengths = id_strengths.drop(['location','psychometric_score','presentation_score'],axis=1)
+    id_strengths = id_strengths.drop(columns=['name'])
+    id_strengths = id_strengths.drop(columns=['date'])
+    id_strengths = pd.merge(id_strengths, id_txts.copy(), on=["id"])
+    id_strengths = id_strengths.drop(columns=['date'])
+    id_strengths = id_strengths.rename(columns={"id": "person_id", "strength":"strength_name"})
+    id_strengths = id_strengths.drop(columns=['location','psychometric_score','presentation_score'])
     newDictStrengths = {"strengths": id_strengths}
 
     #add strengths to final dict
@@ -52,17 +58,43 @@ def transform(dict_of_dfs):
 
 
     ## Weaknesses
-    weeknessDf = transform_weaknesses(dict_of_dfs["json"].copy(deep=True))
-    weeknessDf= pd.merge(weeknessDf, txts, on="date")
+    weaknessesDf = transform_weaknesses(dict_of_dfs["json"].copy(deep=True))
 
-    #set strengths to have a person id and remove the names and dates
-    id_weaknesses = set_person_id(weeknessDf,mapping_df,names_freq,course=False)
-    id_weaknesses = id_weaknesses.rename(columns={"id": "person_id"})
-    id_weaknesses = id_weaknesses.drop(['name','date'],axis=1)
+    #set Weaknesses to have a person id and remove the names and dates
+    id_weaknesses = set_person_id(weaknessesDf,mapping_df,names_freq,course=False)
+    id_weaknesses = id_weaknesses.drop(columns=['name'])
+    id_weaknesses = id_weaknesses.drop(columns=['date'])
+    id_weaknesses = pd.merge(id_weaknesses, id_txts.copy(), on=["id"])
+    id_weaknesses = id_weaknesses.drop(columns=['date'])
+    id_weaknesses = id_weaknesses.rename(columns={"id": "person_id", "weakness":"weaknesses_name"})
+    id_weaknesses = id_weaknesses.drop(columns=['location','psychometric_score','presentation_score'])
     newDictWeaknesses = {"weaknesses": id_weaknesses}
 
-    #add strengths to final dict
+    #add Weaknesses to final dict
     finalDict.update(newDictWeaknesses)
+
+
+    ## TechSkills
+    techSkillsDf = get_tech_skills_frame(dict_of_dfs["json"].copy(deep=True))
+
+    #set TechSkills to have a person id and remove the names and dates
+    id_techSkills = set_person_id(techSkillsDf,mapping_df,names_freq,course=False)
+    id_techSkills = id_techSkills.drop(columns=['name'])
+    id_techSkills = id_techSkills.drop(columns=['date'])
+    id_techSkills = pd.merge(id_techSkills, id_txts.copy(), on=["id"])
+    id_techSkills = id_techSkills.drop(columns=['date'])
+    id_techSkills = id_techSkills.rename(columns={"id": "person_id"})
+    id_techSkills = id_techSkills.drop(columns=['location','psychometric_score','presentation_score'])
+    newDictTechSkills = {"tech_skills": id_techSkills}
+
+    #add TechSkills to final dict
+    finalDict.update(newDictTechSkills)
+
+
+    #SpartaDayResults + txt SpartaDayResults
+    json_data_results = get_json_sparta_day_results(dict_of_dfs["json"].copy(deep=True))
+    json_data_results_id = set_person_id(json_data_results,mapping_df,names_freq,course=False)
+    big_sparta_day_table = pd.merge(json_data_results_id, id_txts.copy(), on=["id"])
 
 
     #transform course behaviour csv
